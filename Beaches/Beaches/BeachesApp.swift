f34022377723ca0987e652c65b425518be0a13b3
Arundas MK <mkarundas@gmail.com>
2024-04-14 13:19:13 +0530

    creating project

---
+++ Beaches/Beaches/BeachesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeachesApp.swift
+//  Beaches
+//
+//  Created by Arundas MK on 14/04/24.
+//
+
+import SwiftUI
+
+@main
+struct BeachesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Beaches/Beaches/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Beaches
+//
+//  Created by Arundas MK on 14/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
