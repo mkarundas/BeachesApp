ebc0e21b49e1d58f3313a09852759aa0ad17c228
Arundas MK <mkarundas@gmail.com>
2024-04-14 13:28:05 +0530

    adding model

--- Beaches/Beaches/ContentView.swift
+++ Beaches/Beaches/ContentView.swift
@@ -8,6 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let beaches = [
+        Beach(name: "Lucky Bay", photo: "lucky", country: "Australia", info: "Lucky Bay is a bay located at 33°59′40″S 122°13′57″E on the south coast of Western Australia, in the Cape Le Grand National Park."),
+        Beach(name: "Navagio", photo: "navagio", country: "Greece", info: "Navagio Beach, or Shipwreck Beach, is an exposed cove, sometimes referred to as \"Smugglers Cove\", on the coast of Zakynthos, in the Ionian Islands of Greece."),
+        Beach(name: "Playa Balandra", photo: "balandra", country: "Mexico", info: "Playa Balandra is a beach located on the Baja California Sur peninsula of Mexico in La Paz.")
+    ]
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
---
+++ Beaches/Beaches/Models/Beach.swift
@@ -0,0 +1,17 @@
+//
+//  Beach.swift
+//  Beaches
+//
+//  Created by Arundas MK on 14/04/24.
+//
+
+import Foundation
+
+
+struct Beach {
+    let name: String
+    let photo: String
+    let country: String
+    let info: String
+    
+}
